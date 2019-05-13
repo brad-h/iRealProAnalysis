cbb96bab0d1265e099675c8a2b9b5218a1892bf9
brad-h <brad.hawbaker@gmail.com>
2019-05-13 05:28:03 -0500

    sync both environments - this code isn't "ready"

--- iRealProAnalysis.py
+++ iRealProAnalysis.py
@@ -57,23 +57,46 @@ def split_chords_in_measure(bar):
 Chord = namedtuple("Chord", ["key", "quality"])
 
 def extension_to_quality(extension):
-    return "Dominant"
+    lookup = {
+        "^7": "Major",
+        "6":  "Major",
+        "7":  "Dominant",
+        "-7": "Minor",
+        "h7": "Half-Diminished",
+        "o7": "Diminished"
+    }
+    return lookup[extension]
 
 def parse_chord(chord):
-    (key,extension) = re.findall("([A-G][#b]?)(^7|7)?", chord)
-    return Chord(key[0], extension_to_quality(extension))
+    [(key,extension)] = re.findall(r"([A-G][#b]?)(7|6|\^7|-7|h7|o7)?", chord)
+    return Chord(key, extension_to_quality(extension))
 
 if __name__ == "__main__":
     tunes = get_tunes(JAZZ1350)
     key_frequency = defaultdict(int)
     chart_frequency = defaultdict(list)
+    all_chords = set()
+    
     for tune in tunes:
+        assert isinstance(tune, Tune)
         key_frequency[tune.key] += 1
         chart_frequency[tune.chord_string].append(tune)
+        # chords = (tune.chord_string
+        #     .replace("|x", "")
+        #     .replace("|", " ")
+        #     .replace("{", "")
+        #     .replace("}", "")
+        #     .replace("[", "")
+        #     .replace("]", ""))
+        # chords = re.sub(r"T\d\d", "", chords)
+        # chords = re.sub(r"*[A-Z]", "", chords)
+        for measure in tune.measures_as_strings:
+            all_chords.add(measure)
+    with open("chords.txt", "w") as file:
+        file.writelines(x + "\n" for x in sorted(all_chords))
+    #cgroup = os.linesep.join("; ".join(x.title for x in v) for (k, v) in chart_frequency.items() if len(v) > 1)
     
-    cgroup = os.linesep.join("; ".join(x.title for x in v) for (k, v) in chart_frequency.items() if len(v) > 1)
-    
-    print(cgroup)
+    # print(cgroup)
     print(sorted([(f, k) for (k, f) in key_frequency.items()], reverse=True))
     minor_count = sum(f for (x, f) in key_frequency.items() if x.find("-") >= 0)
     print("Minors: " + str(minor_count))
--- iRealProAnalysis_test.py
+++ iRealProAnalysis_test.py
@@ -51,15 +51,36 @@ class DataTests(TestCase):
         expected = Case(tc.replace("7", ""), "Dominant")
         self.assertEqual(actual, expected)
 
-    @data(*map("".join, product("ABCDEFG", "#b", ["^7"])))
+    @data(*map("".join, product("ABCDEFG", "#b", ["^7", "6"])))
     def test_parse_major_chord_symbol(self, tc):
         actual = parse_chord(tc)
         expected = Case(tc.replace("^7", ""), "Major")
         self.assertEqual(actual, expected)
 
-    @data(*map("".join, product("ABCDEFG", "#b")))
+    @data(*map("".join, product("ABCDEFG", "#b", ["-7"])))
+    def test_parse_minor_chord_symbol(self, tc):
+        actual = parse_chord(tc)
+        expected = Case(tc.replace("-7", ""), "Minor")
+        self.assertEqual(actual, expected)
+
+    @data(*map("".join, product("ABCDEFG", "#b", ["h7"])))
+    def test_parse_halfdiminished_chord_symbol(self, tc):
+        actual = parse_chord(tc)
+        expected = Case(tc.replace("h7", ""), "Half-Diminished")
+        self.assertEqual(actual, expected)
+
+    @data(*map("".join, product("ABCDEFG", "#b", ["o7"])))
+    def test_parse_diminished_chord_symbol(self, tc):
+        actual = parse_chord(tc)
+        expected = Case(tc.replace("o7", ""), "Diminished")
+        self.assertEqual(actual, expected)
+    
+    @data("Bb6/F", "Bb7/D")
+    def test_parse_slash_bass_note(self, tc):
+        pass
+
     @data("D7", "G7", "F7", "Bb6", "C7")
-    def atest_split_single_chord_in_bar(self, tc):
+    def test_split_single_chord_in_bar(self, tc):
         chords = split_chords_in_measure(tc)
         self.assertEqual(len(chords), 1)
     
